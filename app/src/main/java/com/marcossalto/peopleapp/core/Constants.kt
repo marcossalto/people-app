9bf71c35eff7cc3acf5c7959a882ce0cf1920344
Marcos Salto <marcosasalto@gmail.com>
2024-08-15 17:13:21 -0300

    Create packages, constants and strings files

---
+++ app/src/main/java/com/marcossalto/peopleapp/core/Constants.kt
@@ -0,0 +1,9 @@
+package com.marcossalto.peopleapp.core
+
+class Constants {
+    companion object {
+        const val DATABASE_NAME = "people_db"
+        const val USER_TABLE = "user_table"
+        const val USER_ID = "user_id"
+    }
+}
